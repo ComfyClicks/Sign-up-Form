55e2b382d1736c68a713eec27b00f0e321af9553
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-05-15 23:42:56 -0400

    added logic to validate US and internation phone numbers. Added logic to validate both password inputs are the same

--- app.js
+++ app.js
@@ -5,25 +5,22 @@ const email = document.querySelector('#email');
 const phone = document.querySelector('#phone');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
+
 const passwordToggle = document.querySelector('#password + .field-icon');
 const confirmPasswordToggle = document.querySelector('#confirm-password + .field-icon');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   validateEmptyFields();
   validatePasswords(true);
   validateInputs();
 });
 
-function isEmpty(input) {
-  return input.value === '';
-}
-
 function validateEmptyFields() {
   const fields = [firstName, lastName, email, password, confirmPassword];
   
   for (let field of fields) {
-    if (isEmpty(field)) {
+    if (field.value === '') {
       alert('Please fill out all fields.');
       return false;
     }
@@ -32,12 +29,36 @@ function validateEmptyFields() {
   return true;
 }
 
-password.addEventListener('input', function() {
+password.addEventListener('input', () => {
   validatePasswords(false);
 });
-confirmPassword.addEventListener('input', function() {
+
+confirmPassword.addEventListener('input', () => {
   validatePasswords(false);
 });
+
+phone.addEventListener('input', () => {
+  validatePhoneNumber(phone.value);
+});
+
+phone.addEventListener('keypress', (event) => {
+  if (!allowedCharacters(event)) {
+    event.preventDefault();
+  }
+});
+
+function allowedCharacters(event) {
+  var charCode = (event.which) ? event.which : event.keyCode;
+  if ((charCode > 31 && (charCode < 48 || charCode > 57)) && // numeric (0-9)
+      charCode !== 32 && // space  
+      charCode !== 43 && // +
+      charCode !== 45 && // -
+      charCode !== 40 && // (
+      charCode !== 41) { // )
+    return false;
+  }
+  return true;
+}
 
 function validatePasswords(isSubmitEvent) {
   if (password.value !== confirmPassword.value) {
@@ -52,17 +73,39 @@ function validatePasswords(isSubmitEvent) {
   return true;
 }
 
+function validatePhoneNumber(phoneNumber) {
+  const parsedNumber = libphonenumber.parsePhoneNumberFromString(phoneNumber, 'US');
+  const isValid = parsedNumber && parsedNumber.isValid();
+  if (!isValid) {
+    phone.classList.remove('valid');
+    phone.classList.add('invalid');
+  } else {
+    phone.classList.remove('invalid');
+    phone.classList.add('valid');
+  }
+  return isValid;
+}
+
+function validateInputPattern(input, pattern) {
+  const regex = new RegExp(pattern);
+  return regex.test(input.value);
+}
+
 function validateInputs() {
-  if (!validateEmptyFields() || !validatePasswords()) {
+  const emailPattern = '^([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})$';
+  if (!validateEmptyFields() ||
+  !validatePasswords()  ||
+  !validateInputPattern(email, emailPattern) ||
+  !validatePhoneNumber(phone.value)) {
     return;
   }
 }
 
-passwordToggle.addEventListener('click', function() {
+passwordToggle.addEventListener('click', () => {
   togglePasswordVisibility(password, passwordToggle);
 });
 
-confirmPasswordToggle.addEventListener('click', function() {
+confirmPasswordToggle.addEventListener('click', () => {
   togglePasswordVisibility(confirmPassword, confirmPasswordToggle);
 });
 
